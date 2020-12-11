f1b3f2d9fb9dcc83e6dda8960ab769bc3638788d
Raseem <raseem@dexlock.com>
2020-12-11 20:53:12 +0530

    first commit

--- src/apiAction.js
+++ src/apiAction.js
@@ -2,18 +2,7 @@ import axios from "axios";
 
 
 
-export const loginUser = (payload) => {
-  return axios({
-    url: `https://dapi.quikdr.com/authentication `,
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-        // Authorization: "Bearer " ,
-        "api-header-security":"C1kxIHN1D81zT7DXFQINoiQKDRXIMLCWTugbg9CorYg5SIxHsBBLNvNbebCxoC1qWhtx"
-      },
-    data: payload,
-  });
-};
+
 
 export const userDetail = () => {
     return axios({
